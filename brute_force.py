08529ce957aad74f4134ab002c335cff08686866
Call-X <emile.miath.architecte@hotmail.com>
2022-01-21 18:03:54 +0100

    SC 21012022

--- brute_force.py
+++ brute_force.py
@@ -2,7 +2,6 @@ import csv
 import time
 from itertools import combinations as cbts
 
-
 class Dataset:
 
     dataset_list = []
@@ -41,16 +40,20 @@ def brute_force_algorithme(money_wallet):
 
     for i in range(1, len(data_set) + 1):
         for combination in cbts(data_set, i):
+            final_combination = {
+                "action_list": [], 
+                "total_cost": 0,
+                "total_benefit" : 0,   
+            }
             for element in combination:
-                if element[1] <= 0:
-                    pass
-                
-                elif sum(element[1] for element in combination) <= money_wallet:
-                    combinations.append(combination)
-                    """ print(combinations) """
-                
-
-    sorted_combinations = sorted(combinations, key=lambda element: element[3])
+                    
+                if  (final_combination["total_cost"] + element[1]) <= money_wallet:
+                    final_combination["action_list"].append(element)
+                    final_combination["total_cost"] += element[1]
+                    final_combination["total_benefit"] += element[2]
+                    combinations.append(final_combination)
+                              
+    sorted_combinations = max(combinations, key=lambda combination: combination["total_benefit"])
     print(sorted_combinations)
             
 brute_force_algorithme(500)
